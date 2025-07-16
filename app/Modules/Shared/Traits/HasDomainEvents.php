38aeac0732d6ff9de34c5affe6bfd7e9654fdc58
Adam Pawełczyk <atpawelczyk@gmail.com>
2025-07-16 12:43:06 +0200

    Trait do eventów domenowych.

---
+++ app/Modules/Shared/Traits/HasDomainEvents.php
@@ -0,0 +1,30 @@
+<?php
+/** @author: Adam Pawełczyk */
+
+namespace App\Modules\Shared\Traits;
+
+trait HasDomainEvents
+{
+    /** @var array<object> */
+    private array $recordedEvents = [];
+
+    public function recordEvent(object $event): void
+    {
+        $this->recordedEvents[] = $event;
+    }
+
+    /** @return array<object> */
+    public function releaseEvents(): array
+    {
+        $events = $this->recordedEvents;
+        $this->recordedEvents = [];
+
+        return $events;
+    }
+
+    /** @return bool */
+    public function hasEvents(): bool
+    {
+        return !empty($this->recordedEvents);
+    }
+}
